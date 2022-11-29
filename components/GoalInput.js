38c79cef23188aa3f1d3b7062fd72e82adcb5979
Amit <enigmatic.cipher@gmail.com>
2022-11-29 15:38:59 +0530

    to-do-app

---
+++ components/GoalInput.js
@@ -0,0 +1,82 @@
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  Image,
+} from "react-native";
+import { useState } from "react";
+
+const GoalInput = (props) => {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  };
+
+  return (
+    <Modal visible={props.visble} animationType="slide">
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter Text..."
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} color="#03dac5" />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancle" onPress={props.onCancel} color="#f31282" />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: "#6200EE",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marging: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#3700b3",
+    backgroundColor: "white",
+    color: "#000000",
+    borderRadius: 8,
+    width: "90%",
+    marginRight: 8,
+    padding: 10,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
+});
---
+++ components/GoalItem.js
@@ -0,0 +1,28 @@
+import { StyleSheet, View, Text, Pressable } from "react-native";
+
+const GoalItem = (props) => {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#3700b3",
+  },
+  goalText: {
+    color: "white",
+    padding: 8,
+  },
+});
